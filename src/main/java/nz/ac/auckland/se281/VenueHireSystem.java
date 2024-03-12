37a4dd2f5b514adfeeea4b6921fe65024b9d6acd
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-03-12 22:18:50 +1300

    Create arraylists for venue parameters (4) and add basic functionality of createVenue method

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -3,9 +3,32 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
-public class VenueHireSystem {
+import java.util.ArrayList;
+//import java.util.List;
 
-  public VenueHireSystem() {}
+public class VenueHireSystem {
+  ArrayList<String> nameList;
+  ArrayList<String> codeList;
+  ArrayList<String> capList;
+  ArrayList<String> feeList;
+
+  ArrayList<ArrayList<String>> venueList;
+
+  public VenueHireSystem() {
+    //4 arraylists of the venues' data.
+    nameList = new ArrayList<String>();
+    codeList = new ArrayList<String>();
+    capList = new ArrayList<String>();
+    feeList = new ArrayList<String>();
+
+    //Arraylist containing the 4 arraylists of the venues' data in order of their arguments in createVenue.
+    venueList = new ArrayList<ArrayList<String>>();
+    venueList.add(nameList);
+    venueList.add(codeList);
+    venueList.add(capList);
+    venueList.add(feeList);
+
+  }
 
   public void printVenues() {
     // TODO implement this method
@@ -14,6 +37,12 @@ public class VenueHireSystem {
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     // TODO implement this method
+    venueList.get(0).add(venueName);
+    venueList.get(1).add(venueCode);
+    venueList.get(2).add(capacityInput);
+    venueList.get(3).add(hireFeeInput); //COMMIT WITH MESSAGE FIRST
+
+    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
   }
 
   public void setSystemDate(String dateInput) {
